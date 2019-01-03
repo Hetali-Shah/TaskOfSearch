d01dc8aaa17779c626495e1b8a95b991e5a5550d
hetali-shah <hetali1311.shah@gmail.com>
2019-01-03 19:24:29 +0530

    Task

---
+++ demo.js
@@ -0,0 +1,93 @@
+var  dataOne = [
+  {
+    "id": "296",
+    "employee_name": "suresh ",
+    "employee_salary": "5",
+    "employee_age": "301",
+    "profile_image": ""
+  },
+  {
+    "id": "297",
+    "employee_name": "kumar",
+    "employee_salary": "3",
+    "employee_age": "207",
+    "profile_image": ""
+  },
+  {
+    "id": "299",
+    "employee_name": "Arun",
+    "employee_salary": "1",
+    "employee_age": "27",
+    "profile_image": ""
+  },
+  {
+    "id": "301",
+    "employee_name": "suresh ",
+    "employee_salary": "5",
+    "employee_age": "31",
+    "profile_image": ""
+  },
+  {
+    "id": "305",
+    "employee_name": "ramesh ",
+    "employee_salary": "4",
+    "employee_age": "30",
+    "profile_image": ""
+  },
+  {
+    "id": "306",
+    "employee_name": "Arun",
+    "employee_salary": "1",
+    "employee_age": "27",
+    "profile_image": ""
+  },
+  {
+    "id": "307",
+    "employee_name": "ramesh ",
+    "employee_salary": "4",
+    "employee_age": "30",
+    "profile_image": ""
+  },
+  {
+    "id": "308",
+    "employee_name": "suresh ",
+    "employee_salary": "5",
+    "employee_age": "31",
+    "profile_image": ""
+  },
+  {
+    "id": "309",
+    "employee_name": "ramesh ",
+    "employee_salary": "4",
+    "employee_age": "30",
+    "profile_image": ""
+  },
+  {
+    "id": "310",
+    "employee_name": "ramesh ",
+    "employee_salary": "4",
+    "employee_age": "30",
+    "profile_image": ""
+  },
+  {
+    "id": "311",
+    "employee_name": "suresh ",
+    "employee_salary": "5",
+    "employee_age": "31",
+    "profile_image": ""
+  },
+  {
+    "id": "312",
+    "employee_name": "artha",
+    "employee_salary": "2",
+    "employee_age": "28",
+    "profile_image": ""
+  },
+  {
+    "id": "313",
+    "employee_name": "ramesh ",
+    "employee_salary": "4",
+    "employee_age": "30",
+    "profile_image": ""
+  },
+]
